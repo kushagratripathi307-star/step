2a70fe26249f1b6dc53da11b4ff4cc61b0b1edec
kushagratripathi307-star <kushagra.tripathi307@gmail.com>
2026-02-24 15:13:38 +0530

    initial commit

---
+++ uc1.java
@@ -0,0 +1,25 @@
+/**
+ * OOPSBannerApp
+ *
+ * Use Case 1 (UC1):
+ * Prints the literal text "OOPS" to the console.
+ *
+ * This is the entry-level implementation of the OOPS Banner App.
+ *
+ * @author Kushagra Tripathi
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print literal string
+        System.out.println("OOPS");
+
+    }
+}
